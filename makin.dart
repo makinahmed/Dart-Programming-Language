61cb5eeb990a72798947562cc90c1ba101fadb07
Makin <53996363+makinahmed@users.noreply.github.com>
2020-12-26 14:01:16 +0600

    added oop file

---
+++ makin.dart
@@ -0,0 +1,6 @@
+import 'man.dart';
+
+void main() {
+  var obj = new Man();
+  obj.isEat();
+}
---
+++ man.dart
@@ -0,0 +1,9 @@
+class Man {
+  var hands = 2;
+  var leg = 2;
+  var eye = 2;
+
+  void isEat() {
+    print("Yes");
+  }
+}
